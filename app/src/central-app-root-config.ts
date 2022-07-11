250eeff43efef8821d265017dc0d5c0865a3d440
Erick Zanetti <erick_98zanetti_98@hotmail.com>
2022-07-11 17:16:29 -0300

    .

---
+++ app/src/central-app-root-config.ts
@@ -0,0 +1,15 @@
+import { registerApplication, start } from "single-spa";
+
+registerApplication({
+  name: "angular-finance-control",
+  app: () => System.import("angular-finance-control"),
+  activeWhen: (location) => location.pathname.startsWith("/finance-control"),
+});
+
+registerApplication({
+  name: "angular-investment-control",
+  app: () => System.import("angular-investment-control"),
+  activeWhen: (location) => location.pathname.startsWith("/investment-control"),
+});
+
+start();
