81211aaacb0394a600fae6f90363cb10d2d13f00
Erick Zanetti <erick_98zanetti_98@hotmail.com>
2022-08-30 17:35:44 -0300

    reat home

--- app/src/central-app-root-config.ts
+++ app/src/central-app-root-config.ts
@@ -1,27 +1,16 @@
 import { Application, registerApplication, start } from "single-spa";
-import {
-  constructApplications,
-  constructRoutes,
-  constructLayoutEngine,
-} from "single-spa-layout";
-import microfrontendLayout from "./microfrontend-layout.html";
-
-const routes = constructRoutes(microfrontendLayout);
-const applications = constructApplications({
-  routes,
-  loadApp({ name }) {
-    return System.import(name);
-  },
-});
-const layoutEngine = constructLayoutEngine({ routes, applications });
-
-applications.forEach(registerApplication);
-layoutEngine.activate();
 
 registerApplication(
   '@central-app/financial-management',
   (() => System.import('@central-app/financial-management')) as Application<{}>,
   location => location.pathname.startsWith('/financial-management'),
 );
+
+registerApplication(
+  '@central-app/home',
+  (() => System.import('@central-app/home')) as Application<{}>,
+  location => location.pathname.startsWith('/home') || location.pathname === "/" || location.pathname === "",
+);
+
 
 start();
